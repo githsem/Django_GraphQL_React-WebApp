4fa85070aea087212c535918ab7ef3ce5d076610
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-24 13:37:14 +0200

    updated

--- Backend/myproject/myproject/settings.py
+++ Backend/myproject/myproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'graphene_django',
     'myapp',
+    'corsheaders',
 ]
 
 GRAPHENE = {
@@ -53,6 +54,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+    # your React App domain
 ]
 
 ROOT_URLCONF = 'myproject.urls'
