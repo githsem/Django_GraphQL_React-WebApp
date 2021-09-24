d3c043a37f1620f6718a2043e15fb3b59f254ec7
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-24 13:36:57 +0200

    updated

--- Backend/myproject/myapp/models.py
+++ Backend/myproject/myapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class UserModel(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
