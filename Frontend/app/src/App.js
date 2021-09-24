509ef9e7c012888ec5c3423004f8ba639c1260ce
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-24 13:37:36 +0200

    updated

---
+++ Frontend/app/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { UserInfo, CreateUser } from './User'
+
+
+const client = new ApolloClient({
+    uri: 'http://localhost:8000/graphql/',
+});
+
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div style={{
+      backgroundColor: '#00000008',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems:'center',
+      height: '100vh',
+      flexDirection: 'column'
+    }}>
+      <h2>My WebApp</h2>
+      <CreateUser/>
+      <UserInfo/>
+
+    </div>
+  </ApolloProvider>
+
+);
+
+export default App;
